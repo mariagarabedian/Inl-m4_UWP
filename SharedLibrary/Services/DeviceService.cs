853e53d7ad473469ba59377c2f46f103f63f0e3f
MARIA GARABEDIAN <garabedian77@hotmail.com>
2020-10-01 10:05:17 +0200

    Inl-m4_UWP

---
+++ SharedLibrary/Services/DeviceService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using SharedLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using MAD = Microsoft.Azure.Devices;
+
+
+namespace SharedLibrary.Services
+{
+    public static class DeviceService
+    {
+        private static HttpClient _client;
+
+        public static async Task<string> SendMessageAsync(DeviceClient deviceClient)
+        {
+
+             _client = new HttpClient();
+
+            var response = await _client.GetAsync("http://api.openweathermap.org/data/2.5/weather?q=Kumla,se&APPID=340a1c7e1eb2c2fac4b365398b20c7e8");
+            var data = JsonConvert.DeserializeObject<TemperatureModel.Temperature>(await response.Content.ReadAsStringAsync());
+            var weather = new WeatherModel
+            {
+                Temperature = data.main.temp,
+                Humidity = data.main.humidity
+            };
+
+            var json = JsonConvert.SerializeObject(weather);
+
+            var payload = new Message(Encoding.UTF8.GetBytes(json));
+            await deviceClient.SendEventAsync(payload);
+
+            Console.WriteLine($"Message sent: {json}");
+
+            return json;
+
+
+
+        }
+
+
+        public static async Task ReceiveMessageAsync(DeviceClient deviceClient)
+        {
+            while (true)
+            {
+                var payload = await deviceClient.ReceiveAsync();
+
+                if (payload == null)
+                    continue;
+
+                Console.WriteLine($"Message Received: {Encoding.UTF8.GetString(payload.GetBytes())}");
+
+                await deviceClient.CompleteAsync(payload);
+            }
+
+
+        }
+
+        public static async Task SendMessageToDeviceAsync(MAD.ServiceClient serviceClient, string targetDeviceId, string message)
+        {
+            var payload = new MAD.Message(Encoding.UTF8.GetBytes(message));
+            await serviceClient.SendAsync(targetDeviceId, payload);
+        }
+
+
+    }
+}
---
+++ UWP/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using SharedUwpLibraries.Models;
+using SharedUwpLibraries.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWP
+{
+   
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private static readonly string _conn = "HostName=ec-win20-min-iothub.azure-devices.net;DeviceId=consoleapp;SharedAccessKey=FFKKKzILXO5ti7goQ1Jxzg1eaAuDpszzd/pO5hawu78=";
+        private static readonly DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(_conn, TransportType.Mqtt);
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public WeatherList weatherList = new WeatherList();
+
+        private async void btnGetTemperature_Click(object sender, RoutedEventArgs e)
+        {
+            var result = await DeviceService.SendMessageAsync(deviceClient);
+
+            
+
+            try
+            {
+                weatherList.Add(new WeatherModel(result,""));
+                lvWeatherList.ItemsSource = weatherList;
+            }
+            catch { }
+        }
+
+
+    }
+}
